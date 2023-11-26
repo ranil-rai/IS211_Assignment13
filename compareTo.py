285ed364a4517efccf0ac20abbc935abda38d368
ranil-rai <ranil.rai72@spsmail.cuny.edu>
2023-11-25 19:17:58 -0500

    adding assignment 13

---
+++ compareTo.py
@@ -0,0 +1,12 @@
+
+def compareTo(s1, s2):
+    if not s1 and not s2:
+        return 0
+    if not s1:
+        return -ord(s2[0])
+    if not s2:
+        return ord(s1[0])
+    if s1[0] != s2[0]:
+        return ord(s1[0]) - ord(s2[0])
+    else:
+        return compareTo(s1[1:], s2[1:])
